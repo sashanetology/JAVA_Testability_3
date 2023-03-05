e6d10ad4001276afec85e87acc46a32f36f88fc5
Саша <friend_adversary@mail.ru>
2023-03-06 00:11:35 +0300

    Second Commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class CreditPaymentService {
+public class CreditPaymentService {
+    public double calculate(double sum, double annualRate, double period) {
+        double monthlyPay;
+        double monthlyRate = annualRate / (100 * 12);
+        monthlyPay = sum * (Math.pow((monthlyRate + 1), period) * monthlyRate / (Math.pow((monthlyRate + 1), period) - 1));
+        return monthlyPay;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        double sum = 1_000_000;
+        double annualRate = 9.99;
+        double period = 36;
+        double payment = service.calculate(sum, annualRate, period);
+        System.out.println(Math.round(payment));
+    }
 }
